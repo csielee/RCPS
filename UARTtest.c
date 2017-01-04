e80b03598268544744753adb1014d7649fad0960
csielee <F74031027@mail.ncku.edu.tw>
2017-01-05 01:29:32 +0800

    UART測試用code
    UCA0 baud 9600
    UCA1 baud 9600

---
+++ UARTtest.c
@@ -0,0 +1,91 @@
+
+#include "io430.h"
+
+int main( void )
+{
+  // Stop watchdog timer to prevent time out reset
+  WDTCTL = WDTPW + WDTHOLD;
+  //DMA
+  //DMACTL0 = DMA0TSEL_
+  
+  //UCA0
+  P3SEL = BIT3+BIT4;                        // P3.4,5 = USCI_A0 TXD/RXD
+  UCA0CTL1 |= UCSWRST;                      // **Put state machine in reset**
+  UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+  UCA0BR0 = 6;                              // 1MHz 9600 (see User's Guide)
+  UCA0BR1 = 0;                              // 1MHz 9600
+  UCA0MCTL = UCBRS_0 + UCBRF_13 + UCOS16;   // Modln UCBRSx=0, UCBRFx=0,
+                                            // over sampling
+  UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+  UCA0IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+  //UCA1
+  P4SEL = BIT4+BIT5;                        // P3.4,5 = USCI_A0 TXD/RXD
+  UCA1CTL1 |= UCSWRST;                      // **Put state machine in reset**
+  UCA1CTL1 |= UCSSEL_2;                     // SMCLK
+  UCA1BR0 = 6;                              // 1MHz 9600 (see User's Guide)
+  UCA1BR1 = 0;                              // 1MHz 9600
+  UCA1MCTL = UCBRS_0 + UCBRF_13 + UCOS16;   // Modln UCBRSx=0, UCBRFx=0,
+                                            // over sampling
+  UCA1CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+  UCA1IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+  
+  
+  __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, interrupts enabled
+  __no_operation();                         // For debugger
+
+  return 0;
+}
+//UCA0
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+{
+  switch(__even_in_range(UCA0IV,4))
+  {
+  case 0:break;                             // Vector 0 - no interrupt
+  case 2:                                   // Vector 2 - RXIFG
+    //while (!(UCA0IFG&UCTXIFG));             // USCI_A0 TX buffer ready?
+    //UCA0TXBUF = UCA0RXBUF;                  // TX -> RXed character
+    while (!(UCA1IFG&UCTXIFG));             // USCI_A0 TX buffer ready?
+    UCA1TXBUF = UCA0RXBUF;                  // TX -> RXed character
+    break;
+  case 4:break;                             // Vector 4 - TXIFG
+  default: break;
+  }
+}
+//UCA1
+#pragma vector=USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void)
+{
+  switch(__even_in_range(UCA1IV,4))
+  {
+  case 0:break;                             // Vector 0 - no interrupt
+  case 2:                                   // Vector 2 - RXIFG
+    //while (!(UCA1IFG&UCTXIFG));             // USCI_A0 TX buffer ready?
+    //UCA1TXBUF = UCA1RXBUF;                  // TX -> RXed character
+    while (!(UCA0IFG&UCTXIFG));             // USCI_A0 TX buffer ready?
+    UCA0TXBUF = UCA1RXBUF;                  // TX -> RXed character
+    break;
+  case 4:break;                             // Vector 4 - TXIFG
+  default: break;
+  }
+}
+
+#pragma vector=DMA_VECTOR
+__interrupt void DMA_ISR(void)
+{
+  switch(__even_in_range(DMAIV,16))
+  {
+    case 0: break;
+    case 2:                                 // DMA0IFG = DMA Channel 0
+                              // Toggle P1.0 - PLACE BREAKPOINT HERE AND CHECK DMA_DST VARIABLE
+      break;
+    case 4: break;                          // DMA1IFG = DMA Channel 1
+    case 6: break;                          // DMA2IFG = DMA Channel 2
+    case 8: break;                          // DMA3IFG = DMA Channel 3
+    case 10: break;                         // DMA4IFG = DMA Channel 4
+    case 12: break;                         // DMA5IFG = DMA Channel 5
+    case 14: break;                         // DMA6IFG = DMA Channel 6
+    case 16: break;                         // DMA7IFG = DMA Channel 7
+    default: break;
+  }
+}
