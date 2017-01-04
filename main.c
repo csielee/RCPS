41af6e14ed5c702f1ec2338308624f4da8879a5e
csielee <F74031027@mail.ncku.edu.tw>
2017-01-05 01:21:07 +0800

    ver0.1
    完成接受指令，處理指令，並回傳

--- main.c
+++ main.c
@@ -5,14 +5,26 @@
 #include <string.h>
 
 #define STRING_SIZE 512
-
+#define FREQUENCY  1048576
+#define LOOPBODY 8 
+#define LOOPCNT 260 
 
 int rx0_f,rx0_b;
 char rx0_str[STRING_SIZE];
 int rx1_f,rx1_b;
 char rx1_str[STRING_SIZE];
+char use_str[STRING_SIZE];
 
 
+void DelayMs(unsigned int ms)
+{
+  unsigned int iq0, iq1;
+  for (iq0=ms; iq0>0; iq0--)
+  {
+    for (iq1=LOOPCNT; iq1>0; iq1--);
+      
+  }
+} 
 
 int UCA0print(const char *str)
 {
@@ -54,21 +66,42 @@ void UCA0readstr(char * str) {
   str[count] = 0;
 }
 
+void UCA0readwait()
+{
+  int temp = rx0_b,count=0;
+  while(count < 5)
+  {
+    DelayMs(100);
+    if(rx0_b==temp)
+      count++;
+    else
+      count = 0;
+    temp = rx0_b;
+  }
+}
+
 void UCA0clear()
 {
   rx0_b = (rx0_f + 1) % STRING_SIZE;
 }
 
-int UCA0find(const char *str)
+int UCA0find(const char *str,int timeout)
 {
   int count=0,len = strlen(str);
+  unsigned int t;
   char ch;
   while(1)
   {
-    while(UCA0available()==0);
+    t = 0;
+    while(UCA0available()==0)
+    {
+      if(timeout>0)
+        t++;
+      if(t>60000)
+        return 0;
+    }
     ch = UCA0read();
-    //if(ch == '\n')
-      //return 0;
+
     if(ch == *(str+count))
     {
       count++;
@@ -157,131 +190,118 @@ int UCA1find(const char *str)
 #define ESPreadstr UCA0readstr
 #define ESPavailable UCA0available
 #define ESPclear UCA0clear
+#define ESPreadwait UCA0readwait
+#define ESPrxbuf rx0_str
+#define ESPrx_f rx0_f
+#define ESPrx_b rx0_b
+
+int ESP_OK();
+int ESP_receiveData(char* str);
+int ESP_sendData(int id,char *msg);
+int ESP_sendCommand(char *command);
+
+char send_str[STRING_SIZE];
+
+int ESP_receiveData(char* str)
+{
+  int id,length;
+
+  ESPfind("+IPD",0);
+  ESPreadstr(send_str);
+  //debug
+  UCA1print(send_str);
+  //debug
+  sscanf(send_str,",%d,%d:GET /%s",&id,&length,str);
+  //debug
+  sprintf(send_str,"id : %d,length : %d,str : [%s]\r\n",id,length,str);
+  UCA1print(send_str);
+  //debug
+  ESPreadwait();
+  ESPclear();
+  return id;
+}
+
+int ESP_sendData(int id,char *msg)
+{
+  int len = strlen(msg),re,count;
+  char command[30];
+  sprintf(send_str,"HTTP/1.1 200 OK\r\nLocation: http://192.168.4.1/\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: %d\r\n\r\n%s",len,msg);
+  len = strlen(send_str);
+  sprintf(command,"AT+CIPSEND=%d,%d\r\n",id,len);
+  do {
+    while(ESP_sendCommand(command)==0)
+      DelayMs(100);
+    count++;
+    if(count > 100)
+      return 3;
+  }while(ESPfind(">",1)==0);
+  
+  ESPprint(send_str);
+  DelayMs(500);
+  re = ESP_OK();
+  ESPclear();
+  return re;
+}
+
+int ESP_sendCommand(char *command)
+{
+  if(command[strlen(command)-1]=='@')
+  command[strlen(command)-1]='?';
+  ESPprint(command);
+  if(command[strlen(command)-1]!='\n')
+    ESPprint("\r\n");
+  
+  return 1;
+}
 
 int ESP_OK()
 {
   static char OK[]="OK\r\n";
   static char ERROR[]="ERROR\r\n";
-  int OK_l=strlen(OK),ERROR_l=strlen(ERROR);
-  int i=0,j=0;
+  static char FAIL[]="FAIL\r\n";
+  static int OK_l=4,ERROR_l=7,FAIL_l=6;
+  int i=0,j=0,k=0,v=0;
   char ch;
-  while(1)
+  while(1>0)
   {
-    while(!(ESPavailable()>0));
-    ch = ESPread();
-    if(ch == *(OK+i))
+    while(ESPavailable()==0)
+    { }
+    v++;
+    ch = ESPrxbuf[(ESPrx_f+v)%STRING_SIZE];
+    if(ch == *(OK+i) )
       i++;
     else
+    {
       i=0;
-    if(ch == *(ERROR+j))
+      if(ch == *(OK+i) )
+        i++;
+    }
+    if(ch == *(ERROR+j) )
       j++;
     else
-      j=0;
-    if(i >= OK_l)
-      return 1;
-    if(j >= ERROR_l)
-      return 0;
-  }
-}
-
-int ESP_sendData(int id,const char *msg)
-{
-  int len = strlen(msg);
-  char str[30];
-  sprintf(str,"AT+CIPSEND=%d,%d\r\n",id,len);
-  ESPprint(str);
-  ESPfind(">");
-  ESPprint(msg);
-  //ESPfind("OK\r\n");
-  
-  return ESP_OK();
-}
-
-char temp1[100],temp2[100];
-
-void ESP_begin()
-{
-  char str[200],temp[100];
-  int re,id,length,connect=0;
-  //檢查狀態
-  ESPprint("AT+CIPSTATUS\r\n");
-  ESPfind("STATUS:");
-  ESPreadstr(str);
-  ESPfind("OK\r\n");
-  int status = str[0] - '0';//get status
-  switch(status)
-  {
-  case 2://has IP
-    UCA1print("has connect\n");
-    break;
-  case 5://No connect wifi
-    UCA1print("No connect\r\n");
-    ESPprint("AT+CIPMUX=1\r\n");
-    re = ESP_OK();
-    sprintf(str,"COPMUX:%d\r\n",re);
-    UCA1print(str);
-    ESPprint("AT+CIPSERVER=1,80\r\n");
-    sprintf(str,"CIPSERVER:%d\r\n",re);
-    UCA1print(str);
-    UCA1print("wait SSID&PWD\r\n");
-    while(!connect)
     {
-      ESPfind("+IPD");
-      ESPreadstr(str);
-      sscanf(str,",%d,%d:GET %s",&id,&length,temp);
-      re = sscanf(temp,"/?SSID=%[^&]&PWD=%s",temp1,temp2);
-      sprintf(str,"id : %d,length : %d,str : [%s],re : %d\r\n",id,length,temp,re);
-      UCA1print(str);
-      //ESPfind("\r\n\r\n");
-      if(1)
-      {
-        //while(ESP_sendData(id,"<html><head><title>RCPS wifi config</title></head><body><form style=text-align:center; method=get><br><h2>RCPS wifi 連線設定</h2><br>SSID : <input type=text name=SSID><br><br>PWD  : <input type=password name=PWD><br><br><input type=submit value=RCPS連線設定></form>	</body></html>")==0);
-        while(ESP_sendData(id,"HTTP/1.1 200 OK\r\nLocation: http://192.168.4.1/\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 4\r\n\r\ntest")==0);
-                //while(ESP_sendData(id,"<html>test<br>test</html>")==0);
-        sprintf(str,"AT+CIPCLOSE=%d\r\n",id);
-        ESPprint(str);
-        ESP_OK();
-      }
-      else
-      {
-        //connect wifi
-        sprintf(str,"AT+CWJAP_DEF=\"%s\",\"%s\"\r\n",temp1,temp2);
-        ESPprint(str);
-        ESPreadstr(str);
-        if(strlen(str) > 4)
-        {
-          re = str[8] - '0';
-          sprintf(temp,"error : %d\r\n",re);
-          UCA1print(temp);
-          __no_operation();
-        }
-        else
-        {
-          ESPreadstr(str);
-          sprintf(str,"AT+CIFSR\r\n");
-          ESPfind("CIFSR:");
-          ESPfind("CIFSR:");
-          ESPreadstr(str);
-          re = strlen(str);
-          str[re-2]='\0';
-          sprintf(temp,"<div style=text-align:center;>connect success, IP = %s</div>",str);
-          connect = 1;
-        }
-        ESPclear();
-        while(ESP_sendData(id,temp)==0);
-        sprintf(str,"AT+CIPCLOSE=%d\r\n",id);
-        ESPprint(str);
-        ESP_OK();
-      }
+      j=0;
+      if(ch == *(ERROR+j) )
+        j++;
     }
-    break;
-  default:
-    break;
+    if(ch == *(FAIL+k) )
+      k++;
+    else
+    {
+      k=0;
+      if(ch == *(FAIL+k) )
+        k++;
+    }
+    
+    if(j == ERROR_l)
+      return 0;
+    if(k == FAIL_l)
+      return -1;
+    if(i == OK_l)
+      return 1;
   }
-  UCA0clear();
+  return 2;
 }
-
-
 
 int main( void )
 {
@@ -314,27 +334,54 @@ int main( void )
   rx0_b=1;
   rx1_f=0;
   rx1_b=1;
+  int id,re,i;
+  char command[50];  
   
-    
-  
-  //loop
   __bis_SR_register(GIE);       // Enter interrupts enabled
   __no_operation();                         // For debugger
-  ESP_begin();
+  
+  ESPprint("AT+CIPMUX=1\r\n");
+  re = ESP_OK();
+  ESPclear();
+  sprintf(command,"COPMUX:%d\r\n",re);
+  UCA1print(command);
+  ESPprint("AT+CIPSERVER=1,80\r\n");
+  re = ESP_OK();
+  ESPclear();
+  sprintf(command,"CIPSERVER:%d\r\n",re);
+  UCA1print(command);
+  
+  //loop
   while(1)
   {
-    if(UCA1available()>0)
+    id = ESP_receiveData(command);
+    //execute command
+    while(ESP_sendCommand(command)==0)
+      DelayMs(100);
+    //get respand
+    //DelayMs(4000);
+    //ESPreadwait();
+    if(command[3]=='C' && command[4]=='W')
+      DelayMs(4000);
+    else
+      DelayMs(500);
+    re = ESP_OK();
+    sprintf(use_str,"%s:%d\r\n",command,re);
+    UCA1print(use_str);
+    for(i=0;ESPavailable();i++)
     {
-      UCA1readstr(temp1);
-      UCA0print(temp1);
+      use_str[i] = ESPread();
     }
-    if(UCA0available()>0)
+    use_str[i] = '\0';
+    while(ESP_sendData(id,use_str)!=1) // not OK
     {
-      UCA0readstr(temp1);
-      UCA1print("[");
-      UCA1print(temp1);
-      UCA1print("]\n");
+      UCA1print("send fail,try again\r\n");
     }
+    UCA1print("send OK\r\n");
+    sprintf(command,"AT+CIPCLOSE=%d\r\n",id);
+    ESPprint(command);
+    ESP_OK();
+    ESPclear();
   }
   //__bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, interrupts enabled
   //__no_operation();                         // For debugger
